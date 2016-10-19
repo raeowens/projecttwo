2f7f2fd3cfe97cdaa01f57c896c347e4e3f0e56e
Doray Gisele Owens <doray.owens@gmail.com>
2016-10-19 15:44:38 -0400

    show page added

--- shelved/app/controllers/shelfs_controller.rb
+++ shelved/app/controllers/shelfs_controller.rb
@@ -1,11 +1,21 @@
 class ShelfsController < ApplicationController
+
+  def show
+    @shelf = Shelf.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    @shelf = Shelf.new(params[:shelf])
+    @shelf = Shelf.new(shelf_params)
 
     @shelf.save
     redirect_to @shelf
+  end
+
+private
+  def shelf_params
+    params.require(:shelf).permit(:location, :books)
   end
 end
